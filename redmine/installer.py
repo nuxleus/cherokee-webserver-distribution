162ca2778c32dc9815bfcb982759e26c964066de
alo <alo@cherokee-project.com>
2011-04-04 16:37:16 +0000

    Fixes license notices
    git-svn-id: svn://svn.cherokee-project.com/distribution/trunk@6470 5dc97367-97f1-0310-9951-d761b3857238

--- redmine/installer.py
+++ redmine/installer.py
@@ -1,10 +1,26 @@
 # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 #
-# Copyright (c) 2010, Octality S.L. - Eume 6, Villaviciosa de Odon,
-# 28670 Madrid, SPAIN. All rights reserved.
+# Cherokee Distribution
 #
-# Any unauthorized dissemination, distribution or copying is strictly
-# forbidden.
+# Authors:
+#      Alvaro Lopez Ortega <alvaro@alobbs.com>
+#
+# Copyright (C) 2011 Alvaro Lopez Ortega
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of version 2 of the GNU General Public
+# License as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
 #
 
 import os
